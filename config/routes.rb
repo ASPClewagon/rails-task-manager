a05b2b270952c49d5a2ca593c889ffe77015e212
Anne-Sophie Cortez <aspintocortez@gmail.com>
2024-11-12 16:57:56 +0100

    tasks

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,16 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "tasks#index"
+  get "/tasks", to: "tasks#index"
+
+  get '/tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  patch '/tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
